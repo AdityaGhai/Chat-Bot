7316b56e9f006d2e96d4565bb534780857cc7dd8
Aditya Ghai <83689540+AdityaGhai@users.noreply.github.com>
2021-12-13 23:00:30 +0530

    Add files via upload

---
+++ Bot Source Code/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\futurereadyqna-bot-Web-Deploy.pubxml -p:Password=7oLj2rz4lZ82vgTHLqqwCulE45GFsrsr0qWpmEotn60iz9MSsdYz6xwWhNiq
+
